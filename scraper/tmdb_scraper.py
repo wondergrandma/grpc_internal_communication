2cd0f24b14064833c0f0e458ba707466922e1d03
Oliver Bielik <oliverbielik@gmail.com>
2025-07-17 00:07:12 -0600

    feat: added new helper functions and tested scraping and DB insert of film with actors and categories

--- scraper/tmdb_scraper.py
+++ scraper/tmdb_scraper.py
@@ -1,83 +1,167 @@
-from selenium.webdriver.chrome.options import Options
-import requests
-from scraper.scraper_base import ScraperBase
-from selenium.webdriver.support.ui import WebDriverWait
-from scraper.dto import ScrapedFilm
-from selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 import re
 from typing import List, Tuple
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from sqlalchemy.engine.row import Row
+
 from database.models.actor import Actor
+from database.models.category import Category
+from scraper.dto import ScrapedFilm
+from scraper.scraper_base import ScraperBase
 from scraper.utils import Utils
+
 
 class TmdbScraper(ScraperBase):
     def __init__(self, url):
         options = Options()
-        options.add_argument('--headless')
+        options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=options)
         self.driver.get(url)
         self.wait = WebDriverWait(self.driver, 10)
 
     def check_site_availability(self) -> bool:
-        try: 
+        try:
             status: bool = requests.get(self.url).ok
             return status
         except:
             # TODO: Logger
             return False
-    
+
+    # TODO: Check naming of variables -> length, actors
     def scrape(self, searched_film: str) -> ScrapedFilm | bool:
         search = self.driver.find_element(By.ID, "inner_search_v4")
         search.send_keys(searched_film)
         search.send_keys(Keys.RETURN)
-        cards = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.card.v4.tight')))
+        cards_element = self.wait.until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.card.v4.tight"))
+        )
 
-        cards[0].find_element(By.TAG_NAME, "a").click()
+        cards_element[0].find_element(By.TAG_NAME, "a").click()
 
-        title = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/h2/a')))
-        make_year = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/div/span[2]')))
-        #age_restriction = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/div/span[1]')))
-        length = self.extract_time(self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/div/span[4]'))))
-        overview = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[3]/div/p')))
-        actors = self.extract_actors(element=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="cast_scroller"]/ol'))))
+        title_element = self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/h2/a')
+            )
+        )
+        make_year_element = self.wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="original_header"]/div[2]/section/div[1]/div/span[2]',
+                )
+            )
+        )
+        # age_restriction = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="original_header"]/div[2]/section/div[1]/div/span[1]')))
+        length_element = self.extract_time(
+            self.wait.until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="original_header"]/div[2]/section/div[1]/div/span[4]',
+                    )
+                )
+            )
+        )
+        genre_element = self.extract_genre(
+            self.wait.until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//*[@id="original_header"]/div[2]/section/div[1]/div/span[3]',
+                    )
+                )
+            )
+        )
+        overview_element = self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="original_header"]/div[2]/section/div[3]/div/p')
+            )
+        )
+        actors_element = self.extract_actors(
+            element=self.wait.until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="cast_scroller"]/ol')
+                )
+            )
+        )
 
-        s=self.get_actors(actors)
-        for ss in s: 
-            print(f"{ss.Name} {ss.Surname}")
+        Utils.create_film(
+            name=title_element.text,
+            make_year=self.extract_year(make_year_element.text),
+            hour=length_element[0],
+            minute=length_element[1],
+            categories=self.get_categories(genre_element),
+            overview=overview_element.text,
+            actors=self.get_actors(actors_element),
+            director="",
+            writer="",
+            rating=1,
+            cover_path="",
+        )
 
-        Utils.create_film(name=title.text, make_year=self.extract_year(make_year.text), hour=length[0], minute=length[1], 
-                           categories="", overview=overview.text, 
-                           actors=self.get_actors(actors), director="", writer="", rating="", cover_path="")
+        # return ScrapedFilm(Name=title_element.text, MakeYear=self.extract_year(make_year_element.text), Hour=length_element[0], Minute=length_element[1],
+        #                    Categories=self.get_categories(genre_element), Overview=overview_element.text,
+        #                    Actors=self.get_actors(actors_element), Director="", Writer="", Rating=1, CoverPath="")
 
-        return ScrapedFilm(Name=title.text, MakeYear=self.extract_year(make_year.text), Hour=length[0], Minute=length[1], 
-                           Categories="", Overview=overview.text, 
-                           Actors=self.get_actors(actors), Director="", Writer="", Rating="", CoverPath="")
-    
+    def get_categories(self, categories: List[str]) -> List[Category]:
+        categories_list: List[Category] = []
+
+        for genre in categories:
+            temp_gener: Category = Utils.get_category_by_genre(genre)
+
+            if isinstance(temp_gener, Category):
+                categories_list.append(temp_gener)
+            else:
+                created: Tuple[int] = Utils.create_category(genre)
+
+                if isinstance(created, Row):
+                    print("SOM TU")
+                    new_category: Category = Utils.get_category_by_id(created[0])
+                    categories_list.append(new_category)
+
+        return categories_list
+
     def get_actors(self, actors: List[str]) -> List[Actor]:
         actors_list: List[Actor] = []
 
         for actor in actors:
             actor_name = self.extract_name_surname(actor)
-            temp_actor: Actor = Utils.get_actor(name=actor_name[0], surname=actor_name[1])
+            temp_actor: Actor = Utils.get_actor_by_name(
+                name=actor_name[0], surname=actor_name[1]
+            )
 
-            if(isinstance(temp_actor, Actor)):
+            if isinstance(temp_actor, Actor):
                 actors_list.append(temp_actor)
             else:
-                created: bool = Utils.create_actor()
+                created: Tuple[int] = Utils.create_actor(
+                    name=actor_name[0], surname=actor_name[1]
+                )
 
-                if created:
-                    new_actor: Actor = Utils.get_actor(name=actor_name[0], surname=actor_name[1])
+                if isinstance(created, Row):
+                    new_actor: Actor = Utils.get_actor_by_id(created[0])
                     actors_list.append(new_actor)
-        
+
         return actors_list
-            
+
+    def extract_genre(self, genres: WebElement) -> List[str]:
+        genre_elements: List[WebElement] = genres.find_elements(By.TAG_NAME, "a")
+        geners_list: List[str] = []
+
+        for gener in genre_elements:
+            geners_list.append(gener.text)
+
+        return geners_list
+
     def extract_year(self, date: str):
         split_date: List[str] = date.split("/")
-        year: int = int(split_date[2])
+        year: int = int(split_date[2].split(" ")[0])
 
         return year
 
@@ -93,17 +177,19 @@ class TmdbScraper(ScraperBase):
 
     def extract_actors(self, element: WebElement) -> List[str]:
         try:
-            actors_card = element.find_elements(By.CLASS_NAME, 'card')
+            actors_card = element.find_elements(By.CLASS_NAME, "card")
             actors = []
 
             for i in range(4):
-                temp_actor = actors_card[i].find_element(By.XPATH, f'//*[@id="cast_scroller"]/ol/li[{i+1}]/p[1]/a')
+                temp_actor = actors_card[i].find_element(
+                    By.XPATH, f'//*[@id="cast_scroller"]/ol/li[{i+1}]/p[1]/a'
+                )
                 actors.append(temp_actor.text)
-            
+
             return actors
         except Exception as e:
             raise
-    
+
     def extract_name_surname(self, actor: str) -> Tuple[str, str]:
         try:
             split_actor_name: List[str] = actor.split(" ")
@@ -112,7 +198,7 @@ class TmdbScraper(ScraperBase):
             surname: str
 
             for name in split_actor_name:
-                if(name_length == 2):
+                if name_length == 2:
                     name = split_actor_name[0]
                     surname = split_actor_name[1]
                 else:
@@ -120,9 +206,9 @@ class TmdbScraper(ScraperBase):
                     surname = " ".join(split_actor_name[1:name_length])
 
             return name, surname
-        
+
         except Exception as e:
             raise
-    
+
     def close(self):
         self.driver.quit()
