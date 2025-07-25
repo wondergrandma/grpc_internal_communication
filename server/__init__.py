c13500945657fd4b3524003284fa5fe192c90d9b
Oliver Bielik <oliverbielik@gmail.com>
2025-07-24 23:06:20 -0600

    feat: adding image directory to init file

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,8 @@
 from os import getenv
+
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env")
 
 # TMDB SCRAPER VARIABLES
-IMAGE_STORAGE_DIRECTORY=getenv("IMAGE_STORAGE_DIRECTORY")
+IMAGE_STORAGE_DIRECTORY = getenv("IMAGE_STORAGE_DIRECTORY")
